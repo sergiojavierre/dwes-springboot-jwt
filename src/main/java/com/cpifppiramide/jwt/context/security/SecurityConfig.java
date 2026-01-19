202ea96ff7f6719ac3ef20161affc8803f063c07
sergiojavierre <sergio.javierre@fppiramide.com>
2026-01-19 09:42:37 +0100

    Documentación swagger

--- src/main/java/com/cpifppiramide/jwt/context/security/SecurityConfig.java
+++ src/main/java/com/cpifppiramide/jwt/context/security/SecurityConfig.java
@@ -24,6 +24,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
